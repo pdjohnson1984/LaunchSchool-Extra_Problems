746e36de427f4079a2a4bd21b7f2af7db5c7603b
Phillip Johnson <pdjohnson84@gmail.com>
2016-09-27 18:50:01 -0700

    Finishing easy2

--- 101-109 Small Problems/easy2.rb
+++ 101-109 Small Problems/easy2.rb
@@ -32,7 +32,7 @@ puts "The total is $#{total}"
 puts "---------------------------"
 
 #When will I Retire?
-puts "What is youre age?"
+puts "What is your age?"
 age = gets.to_i
 
 puts "At what age would you like to retire?"
@@ -47,3 +47,57 @@ puts "You have only #{years_left} years of work to go!"
 puts "---------------------------"
 
 #Greeting a user
+puts "What is your name?"
+name = gets.chomp
+
+if name[-1, 1] == "!"
+  puts "HELLO #{name.gsub('!', '').upcase}. WHY ARE WE SCREAMING?"
+else
+  puts "Hello #{name}."
+end
+puts "---------------------------"
+
+#Odd Numbers
+(1..99).each do |num|
+  puts num if num.odd?
+end
+puts "---------------------------"
+
+#Even Numbers
+(1..99).each do |num|
+  puts num if num.even?
+end
+puts "---------------------------"
+
+#Sum or Product of Consecutive Integers
+puts "Please enter an integer greater than 0:"
+num = gets.to_i
+
+puts "Enter 's' to compute the sum, 'p' to compute the product."
+compute = gets.chomp
+
+if compute == 's'
+  sum = (1..num).reduce(:+)
+  puts "The sum of the integers between 1 and #{num} is #{sum}."
+elsif compute == 'p'
+  product = (1..num).reduce(:*)
+  puts "The product of the integers between 1 and #{num} is #{product}."
+else
+  puts "Please enter a valid operator."
+end
+puts "---------------------------"
+
+#String Assignment
+puts "It prints out BOB BOB"
+puts "---------------------------"
+
+#Mutation
+"Moe
+Larry
+CURLY
+CHEMP
+Harpo
+CHICO
+Groucho
+Zeppo"
+puts "---------------------------"
